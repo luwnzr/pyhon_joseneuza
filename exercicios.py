129f7da9732666e131321da8f92ba6a9d5e7f892
Luane Gabrielly <luanegabrielly2202@gmail.com>
2024-11-29 15:20:02 -0300

    Update exercicios.py

--- exercicios.py
+++ exercicios.py
@@ -817,3 +817,21 @@ else:
 
 # Exibe o vencedor
 print(f"O vencedor da eleição é: {vencedor}")
+
+#Advinhar Numero
+import random
+numero_sorteado = random.randint(1, 100)
+tentativas = 0 
+
+while tentativas < 10:
+    chute = int(input("digite seu chute"))
+    tentativas += 1
+    if chute > numero_sorteado:
+        print ("chute maior que o numero sorteado")
+    if chute < numero_sorteado:
+        print ("chute menor que o numero sorteado")
+    else : 
+        print ("voce acertou")
+        break
+if tentativas ==10: 
+    print ("acabou seus chutes, o numero correto era", numero_sorteado)
