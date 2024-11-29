2026992628cb260f80de2bfbf613ee28854c23cc
Luane Gabrielly <luanegabrielly2202@gmail.com>
2024-11-29 15:29:50 -0300

    Update exercicios.py

--- exercicios.py
+++ exercicios.py
@@ -824,12 +824,12 @@ numero_sorteado = random.randint(1, 100)
 tentativas = 0 
 
 while tentativas < 10:
-    chute = int(input("digite seu chute"))
+    chute = int(input("digite seu chute: "))
     tentativas += 1
     if chute > numero_sorteado:
-        print ("chute maior que o numero sorteado")
+        print ("chute maior que o numero sorteado: ")
     if chute < numero_sorteado:
-        print ("chute menor que o numero sorteado")
+        print ("chute menor que o numero sorteado: ")
     else : 
         print ("voce acertou")
         break
